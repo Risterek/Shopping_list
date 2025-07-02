0bd44b99f1ad327465c43899fcd4ba95ec682b47
Risterek <96064064+Risterek@users.noreply.github.com>
2025-07-02 22:52:49 +0200

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -10,25 +10,41 @@ const FILES_TO_CACHE = [
   '/Shopping_list/manifest.json'
 ];
 
+// Adres API pogodowego
 const API_URL = 'https://api.openweathermap.org/data/2.5/weather';
 
 self.addEventListener('install', evt => {
+  console.log('[ServiceWorker] Install');
   evt.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(FILES_TO_CACHE))
+    caches.open(CACHE_NAME).then(cache => {
+      console.log('[ServiceWorker] Caching app shell');
+      return cache.addAll(FILES_TO_CACHE);
+    })
   );
   self.skipWaiting();
 });
 
 self.addEventListener('activate', evt => {
+  console.log('[ServiceWorker] Activate');
   evt.waitUntil(
-    caches.keys().then(keys => Promise.all(
-      keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))
-    ))
+    caches.keys().then(keyList => {
+      return Promise.all(
+        keyList.map(key => {
+          if (key !== CACHE_NAME) {
+            console.log('[ServiceWorker] Removing old cache', key);
+            return caches.delete(key);
+          }
+        })
+      );
+    })
   );
   self.clients.claim();
 });
 
 self.addEventListener('fetch', evt => {
+  const requestUrl = new URL(evt.request.url);
+
+  // Obsługa zapytań do OpenWeather API
   if (evt.request.url.startsWith(API_URL)) {
     evt.respondWith(
       caches.open(CACHE_NAME).then(cache =>
@@ -43,18 +59,18 @@ self.addEventListener('fetch', evt => {
     return;
   }
 
+  // Obsługa nawigacji (np. wpisywanie URL w pasku adresu)
   if (evt.request.mode === 'navigate') {
     evt.respondWith(
-      fetch(evt.request).catch(() =>
-        caches.match('/Shopping_list/offline.html')
-      )
+      fetch(evt.request).catch(() => caches.match('/Shopping_list/offline.html'))
     );
     return;
   }
 
+  // Obsługa pozostałych zasobów (CSS, JS, HTML, manifest)
   evt.respondWith(
-    caches.match(evt.request).then(cachedResponse =>
-      cachedResponse || fetch(evt.request)
-    )
+    caches.match(evt.request).then(response => {
+      return response || fetch(evt.request);
+    })
   );
 });
